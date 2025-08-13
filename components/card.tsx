cbdd702d45a2d6769e80c9018482568677227a6d
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-08-13 20:40:43 +0200

    Half way through font part of portfolio - the linking isn't working. Look at that later

--- components/card.tsx
+++ components/card.tsx
@@ -1,22 +1,30 @@
 'use client'
 import { Card, CardHeader, CardBody, Image } from "@heroui/react";
+import Link from "next/link";
 
-export default function CardPortfolio() {
+interface CardPortfolioProps {
+    source: string;
+    link: string;
+}
+
+export default function CardPortfolio({ source, link }: CardPortfolioProps) {
     return (
-        <Card className="py-4 text-white">
-            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                <p className="text-large uppercase font-bold">Daily Mix</p>
-                <small className="text-default-500">12 Tracks</small>
-                <h4 className="font-bold text-large">Frontend Radio</h4>
-            </CardHeader>
-            <CardBody className="overflow-visible py-2">
-                <Image
-                    alt="Card background"
-                    className="object-cover rounded-xl"
-                    src="https://heroui.com/images/hero-card-complete.jpeg"
-                    width={350}
-                />
-            </CardBody>
-        </Card>
+        <Link href={link} className="block">
+            <Card className="py-4 text-white">
+                <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                    <p className="text-large uppercase font-bold">Daily Mix</p>
+                    <small className="text-default-500">12 Tracks</small>
+                    <h4 className="font-bold text-large">Frontend Radio</h4>
+                </CardHeader>
+                <CardBody className="overflow-visible py-2">
+                    <Image
+                        alt="Card background"
+                        className="object-cover rounded-xl"
+                        src={source}
+                        width={350}
+                    />
+                </CardBody>
+            </Card>
+        </Link>
     );
 }
--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -1,12 +1,11 @@
 import CardPortfolio from "./card"
-import Link from "next/link"
 
 export default function Portfolio() {
     return (
         <div className="relative bg-[#000e2f] py-24 sm:py-32 text-white">
             <div
                 aria-hidden="true"
-                className="absolute inset-x-0 -z-40 -top-40 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+                className="absolute inset-x-0 -z-100 -top-40 transform-gpu overflow-hidden blur-3xl sm:-top-80"
             >
                 <div
                     style={{
@@ -22,11 +21,9 @@ export default function Portfolio() {
                     <p className="mt-2 text-lg/8">Learn how to grow your business with our expert advice.</p>
                 </div>
                 <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                    <Link href="/portfolio">
-                        <CardPortfolio />
-                    </Link>
-                    <CardPortfolio />
-                    <CardPortfolio />
+                        <CardPortfolio link="/portfolio" source="portfolio-logo-0.png" />
+                    <CardPortfolio link="/portfolio" source="portfolio-logo-2.png"/>
+                    <CardPortfolio link="/portfolio" source="portfolio-logo-3.png"/>
                 </div>
             </div>
             <div
