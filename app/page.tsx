3b68850a5c5c0e86d08d90566d92f4ca72ae6eba
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-08-13 18:56:08 +0200

    Footer added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/hero-section";
 import AboutMe from "@/components/about-me";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <>  
       <Hero />
       <AboutMe />
+      <Footer />
     </>
   );
 }
