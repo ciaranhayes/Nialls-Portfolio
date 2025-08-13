2c20100bd263f4dde924a5942d9924092915db64
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-08-13 19:36:07 +0200

    Portfolio started

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/hero-section";
 import AboutMe from "@/components/about-me";
 import Footer from "@/components/footer";
+import Portfolio from "@/components/portfolio";
 
 export default function Home() {
   return (
     <>  
       <Hero />
       <AboutMe />
+      <Portfolio />
       <Footer />
     </>
   );
