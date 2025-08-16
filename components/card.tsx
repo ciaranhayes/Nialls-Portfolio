b5fa04547c7fa6572857685af4052d89862d89f1
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-08-16 17:49:13 +0200

    Fixed the links - it was with the thingy

--- components/card.tsx
+++ components/card.tsx
@@ -1,30 +1,30 @@
 'use client'
-import { Card, CardHeader, CardBody, Image } from "@heroui/react";
+import Image from "next/image";
 import Link from "next/link";
 
 interface CardPortfolioProps {
     source: string;
     link: string;
+    alt: string;
 }
 
-export default function CardPortfolio({ source, link }: CardPortfolioProps) {
+export default function CardPortfolio({ source, link, alt }: CardPortfolioProps) {
     return (
-        <Link href={link} className="block">
-            <Card className="py-4 text-white">
-                <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                    <p className="text-large uppercase font-bold">Daily Mix</p>
-                    <small className="text-default-500">12 Tracks</small>
-                    <h4 className="font-bold text-large">Frontend Radio</h4>
-                </CardHeader>
-                <CardBody className="overflow-visible py-2">
-                    <Image
-                        alt="Card background"
-                        className="object-cover rounded-xl"
-                        src={source}
-                        width={350}
-                    />
-                </CardBody>
-            </Card>
+        <Link href={link} className="block h-full">
+            <div className="h-full rounded overflow-hidden shadow-lg text-black">
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                    <p className="text-gray-700 text-base">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                </div>
+                <div className="px-6 pt-4 pb-2">
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                </div>
+                <Image className="w-full" src={source} alt={alt} width={300} height={50} />
+            </div>
         </Link>
     );
 }
--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -15,29 +15,29 @@ export default function Portfolio() {
                     className="relative left-[50%] top-[50%] aspect-1155/678 w-144.5 -translate-x-1/2 -translate-y-1/2 rotate-[-90deg] bg-linear-to-tr from-[#38d586] to-[#ffffff] opacity-50 sm:w-288.75"
                 />
             </div>
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8 z-40">
                 <div className="mx-auto max-w-2xl lg:mx-0">
                     <h2 className="text-4xl font-semibold tracking-tight text-pretty sm:text-5xl">My Portfolio</h2>
                     <p className="mt-2 text-lg/8">Learn how to grow your business with our expert advice.</p>
                 </div>
                 <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                        <CardPortfolio link="/portfolio" source="portfolio-logo-0.png" />
-                    <CardPortfolio link="/portfolio" source="portfolio-logo-2.png"/>
-                    <CardPortfolio link="/portfolio" source="portfolio-logo-3.png"/>
+                    <CardPortfolio link="/portfolio" source="/portfolio-logo-0.png" alt="Logo" />
+                    <CardPortfolio link="/portfolio" source="/portfolio-logo-2.png" alt="Logo"/>
+                    <CardPortfolio link="/portfolio" source="/portfolio-logo-3.png" alt="Logo"/>
                 </div>
             </div>
             <div
-                aria-hidden="true"
-                className="absolute left-0 top-1/2 -translate-y-1/2 z-0 transform-gpu overflow-hidden blur-3xl"
-            >
-                <div
-                    style={{
-                        clipPath:
-                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                    }}
-                    className="w-[36.125rem] aspect-[1155/678] -rotate-90 bg-gradient-to-tr from-[#38d586] to-[#ffffff] opacity-50 sm:w-[72.1875rem]"
-                />
-            </div>
+                    aria-hidden="true"
+                    className="absolute inset-x-0 -top-40 z-0 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+                >
+                    <div
+                        style={{
+                            clipPath:
+                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                        }}
+                        className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#38d586] to-[#ffffff] opacity-50 sm:left-[calc(50%-30rem)] sm:w-288.75"
+                    />
+                </div>
         </div>
     )
 }
