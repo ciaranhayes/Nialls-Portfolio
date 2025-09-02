3e5fde7c0c0563cd72c9041a0f50370a2535b9bb
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-09-02 23:08:06 +0200

    more fix

--- components/header.tsx
+++ components/header.tsx
@@ -18,14 +18,16 @@ export default function Header() {
                 <div className="flex lg:flex-1">
                 </div>
                 <div className="flex lg:hidden">
-                    <button
-                        type="button"
-                        onClick={() => setMobileMenuOpen(true)}
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-200"
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <Bars3Icon aria-hidden="true" className="size-6" />
-                    </button>
+                    {!mobileMenuOpen && (
+                        <button
+                            type="button"
+                            onClick={() => setMobileMenuOpen(true)}
+                            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-200"
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                    )}
                 </div>
                 <div className="hidden lg:flex lg:gap-x-12">
                     {navigation.map((item) => (
